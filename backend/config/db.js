2b4ebacff07bc336fa52cee5b77583ff0771d444
NitinSingh <mrsinghnitin0301@gmail.com>
2024-08-19 23:33:26 +0530

    good5

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://NitinSingh108:Namagirinujan1729@cluster0.1yyog.mongodb.net/food_delivery').then(()=>console.log("DB Connected"));
+    await mongoose.connect(`mongodb+srv://NitinSingh108:${process.env.PASSWORD}@cluster0.1yyog.mongodb.net/food_delivery`).then(()=>console.log("DB Connected"));
 }
