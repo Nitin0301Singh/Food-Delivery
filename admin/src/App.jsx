4587563bd5f47b220bf5f29fe9d010202e91f25c
NitinSingh <mrsinghnitin0301@gmail.com>
2024-08-22 01:01:46 +0530

    good18

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
         <Sidebar />
         <Routes>
           <Route path="/add" element={<Add url={url} />} />
-          <Route path="/list" element={<List url={url} />} />
+          <Route path="/list" element={<List url={url} />} /> 
           <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
